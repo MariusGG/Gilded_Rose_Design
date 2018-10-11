73c254a32ce480eef3ad1f6f9ae30b27678dff74
mariusgg <marius.g.guei@gmail.com>
2018-10-11 15:33:45 +0100

    afternoon update

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,44 +2,45 @@ class GildedRose
 
   def initialize(items)
       @items = items
-    end
+  end
 
-    def update_quality()
-      @items.each do |item|
+  def item_limit
+    
+  end
 
-        case item.name
 
-        when "Aged Brie"
-          if item.sell_in > 0
-            item.quality +=  1
+  def update_quality()
+    @items.each do |item|
+
+    case item.name
+
+      when "Aged Brie"
+        if item.sell_in > 0
+          item.quality +=  1
+        else
+          item.quality += 2
+        end
+
+        item.quality = item.quality if item.quality <= 50
+
+        item.sell_in -= 1
+        when "Backstage passes to a TAFKAL80ETC concert"
+          item.quality = if item.sell_in > 10
+            item.quality + 1
+          elsif item.sell_in > 5
+            item.quality + 2
+          elsif item.sell_in > 0
+            item.quality + 3
           else
-            item.quality += 2
+            0
           end
 
           item.quality = item.quality if item.quality <= 50
-
+          item.quality = 50 if item.quality> 50
           item.sell_in -= 1
-        when "Backstage passes to a TAFKAL80ETC concert"
-          item.quality = if item.sell_in > 10
-              item.quality + 1
-            elsif item.sell_in > 5
-              item.quality + 2
-            elsif item.sell_in > 0
-              item.quality + 3
-            else
-              0
-            end
-
-  item.quality = item.quality if item.quality <= 50
-  item.quality = 50 if item.quality> 50
-
-  item.sell_in -= 1
-
 
         when "Sulfuras, Hand of Ragnaros"
           # it does nothing
-        when "Conjured"
-        
         else
           if item.sell_in > 0
             item.quality -= 1
@@ -53,5 +54,5 @@ class GildedRose
           end
         end
       end
-
+# SET UP A BOUND CHECKER WHICH DOESN'T ALLOW THE ITEM TO INCREASE BEYOND 50
 end
