dacb8a560961c24f18f72f4c529b3147ba14ffd8
mariusgg <marius.g.guei@gmail.com>
2018-10-11 12:07:19 +0100

    added 'when' for (conjured, backstage pass)

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,8 +6,10 @@ class GildedRose
 
     def update_quality()
       @items.each do |item|
+
         case item.name
-        when 'Aged Brie'
+
+        when "Aged Brie"
           if item.sell_in > 0
             item.quality +=  1
           else
@@ -17,20 +19,23 @@ class GildedRose
           item.quality = item.quality if item.quality <= 50
 
           item.sell_in -= 1
-        when 'Sulfuras, Hand of Ragnaros'
+        when "Backstage passes to a TAFKAL80ETC concert"
+
+        when "Sulfuras, Hand of Ragnaros"
           # it does nothing
+        when "Conjured"
         else
-      if item.sell_in > 0
-        item.quality -= 1
-      else
-        item.quality = 0
-      end
+          if item.sell_in > 0
+            item.quality -= 1
+          else
+            item.quality = 0
+          end
 
-      item.quality = item.quality if item.quality >= 0
+          item.quality = item.quality if item.quality >= 0
 
-      item.sell_in -= 1
+          item.sell_in -= 1
+          end
+        end
       end
-    end
-  end
 
 end
