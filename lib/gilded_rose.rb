03f5587b1756a32c5ce5f31790b3b1afc5b18347
mariusgg <marius.g.guei@gmail.com>
2018-10-11 12:03:26 +0100

    changed the If statments to a Case, added When 'aged brie'

---
+++ lib/gilded_rose.rb
@@ -0,0 +1,36 @@
+class GildedRose
+
+  def initialize(items)
+      @items = items
+    end
+
+    def update_quality()
+      @items.each do |item|
+        case item.name
+        when 'Aged Brie'
+          if item.sell_in > 0
+            item.quality +=  1
+          else
+            item.quality += 2
+          end
+
+          item.quality = item.quality if item.quality <= 50
+
+          item.sell_in -= 1
+        when 'Sulfuras, Hand of Ragnaros'
+          # it does nothing
+        else
+      if item.sell_in > 0
+        item.quality -= 1
+      else
+        item.quality = 0
+      end
+
+      item.quality = item.quality if item.quality >= 0
+
+      item.sell_in -= 1
+      end
+    end
+  end
+
+end
